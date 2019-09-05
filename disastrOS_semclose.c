1809af7d27786ee87f6fc67ab1300effa12c129b
Jessica <jessica_brandi_@hotmail.it>
2019-09-05 02:31:33 +0200

    semClose

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -8,4 +8,30 @@
 
 void internal_semClose(){
   // do stuff :)
+
+  int sem_fd = running -> syscall_args[0];
+
+  SemDescriptor* sem_des = SemDescriptorList_byFd(&(running -> sem_descriptors), sem_fd);
+
+  //se il sem_fd non è nel processo ritorna un errore
+  if (!sem_des) {
+      running -> syscall_retvalue = -1;
+      return;
+  }
+
+  //rimuovo il descrittore dalla lista dei processi
+  sem_des = (SemDescriptor*) List_detach(&(running -> sem_descriptors), (ListItem*) sem_des);
+  assert(sem_des);
+
+  Semaphore* sem = sem_des -> semaphore;
+
+  // rimuovo il sem descriptor pointer dalla lista dei semafori
+
+  SemDescriptorPtr* sem_des_ptr = (SemDescriptorPtr*) List_detach(&sem -> descriptors, (ListItem*)(sem_des -> ptr));
+  assert(sem_des_ptr);
+  
+  SemDescriptor_free(sem_des);
+  SemDescriptorPtr_free(sem_des_ptr);
+
+  running -> syscall_retvalue = 0;
 }
